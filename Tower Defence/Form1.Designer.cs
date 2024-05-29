02d32b5e52dda8af8ab472c48738a259ad76e768
Liam-Benton08 <liambent383@SDS-L-2BHDF63>
2024-05-29 14:54:48 -0400

    First Save, School.

--- Tower Defence/Form1.Designer.cs
+++ Tower Defence/Form1.Designer.cs
@@ -31,6 +31,11 @@
             this.components = new System.ComponentModel.Container();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
             this.titleLabel = new System.Windows.Forms.Label();
+            this.easyButton = new System.Windows.Forms.Button();
+            this.mediumButton = new System.Windows.Forms.Button();
+            this.hardButton = new System.Windows.Forms.Button();
+            this.insaneButton = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // gameTimer
@@ -41,21 +46,72 @@
             // titleLabel
             // 
             this.titleLabel.AutoSize = true;
-            this.titleLabel.Location = new System.Drawing.Point(333, 70);
+            this.titleLabel.Location = new System.Drawing.Point(371, 168);
             this.titleLabel.Name = "titleLabel";
             this.titleLabel.Size = new System.Drawing.Size(35, 13);
             this.titleLabel.TabIndex = 0;
             this.titleLabel.Text = "label1";
+            // 
+            // easyButton
+            // 
+            this.easyButton.Location = new System.Drawing.Point(197, 260);
+            this.easyButton.Name = "easyButton";
+            this.easyButton.Size = new System.Drawing.Size(75, 23);
+            this.easyButton.TabIndex = 1;
+            this.easyButton.Text = "button1";
+            this.easyButton.UseVisualStyleBackColor = true;
+            // 
+            // mediumButton
+            // 
+            this.mediumButton.Location = new System.Drawing.Point(347, 260);
+            this.mediumButton.Name = "mediumButton";
+            this.mediumButton.Size = new System.Drawing.Size(75, 23);
+            this.mediumButton.TabIndex = 2;
+            this.mediumButton.Text = "button2";
+            this.mediumButton.UseVisualStyleBackColor = true;
+            // 
+            // hardButton
+            // 
+            this.hardButton.Location = new System.Drawing.Point(497, 260);
+            this.hardButton.Name = "hardButton";
+            this.hardButton.Size = new System.Drawing.Size(75, 23);
+            this.hardButton.TabIndex = 3;
+            this.hardButton.Text = "button3";
+            this.hardButton.UseVisualStyleBackColor = true;
+            // 
+            // insaneButton
+            // 
+            this.insaneButton.Location = new System.Drawing.Point(347, 330);
+            this.insaneButton.Name = "insaneButton";
+            this.insaneButton.Size = new System.Drawing.Size(75, 23);
+            this.insaneButton.TabIndex = 4;
+            this.insaneButton.Text = "button4";
+            this.insaneButton.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(665, 37);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.insaneButton);
+            this.Controls.Add(this.hardButton);
+            this.Controls.Add(this.mediumButton);
+            this.Controls.Add(this.easyButton);
             this.Controls.Add(this.titleLabel);
             this.Name = "Form1";
             this.Text = "Tower Defence";
             this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -65,6 +121,11 @@
 
         private System.Windows.Forms.Timer gameTimer;
         private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button easyButton;
+        private System.Windows.Forms.Button mediumButton;
+        private System.Windows.Forms.Button hardButton;
+        private System.Windows.Forms.Button insaneButton;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Tower Defence/Form1.cs
+++ Tower Defence/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
+using System.Threading;
 
 namespace Tower_Defence
 {
@@ -24,8 +26,12 @@ namespace Tower_Defence
 
         private void Form1_Shown(object sender, EventArgs e)
         {
-            this.BackColor = Color.White;
             
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
